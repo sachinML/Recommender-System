ba7ee62e6af77afa0fbb18e451239763b11f1bfd
Sachin Jha <Sjha979@gmail.com>
2023-03-22 21:02:15 +0530

    upated views

--- movierecommendation/views.py
+++ movierecommendation/views.py
@@ -9,45 +9,47 @@ from pyspark.ml.evaluation import RegressionEvaluator
 from pyspark.sql.functions import col
 from pyspark.ml.tuning import ParamGridBuilder, CrossValidator
 from movierecommendation.config import cache
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
 DATA_FOLDER = "ml-latest-small"
 
+class recommend_movies(APIView):
+    permission_classes = (IsAuthenticated,)
 
-@require_GET
-def recommend_movies(request, user_id='1'):
-    # Create a SparkSession
-    spark = SparkSession.builder.appName('MovieRecommendation').getOrCreate()
+    def get(self, request, user_id='1'):
+        spark = SparkSession.builder.appName('MovieRecommendation').getOrCreate()
 
-    # Loading the dataset
-    ratings = spark.read.format('csv').options(header='true', inferSchema='true').load(f'{DATA_FOLDER}//ratings.csv').cache()
-    movies = spark.read.format('csv').options(header='true', inferSchema='true').load(f'{DATA_FOLDER}//movies.csv').cache()
+        # Loading the dataset
+        ratings = spark.read.format('csv').options(header='true', inferSchema='true').load(f'{DATA_FOLDER}//ratings.csv').cache()
+        movies = spark.read.format('csv').options(header='true', inferSchema='true').load(f'{DATA_FOLDER}//movies.csv').cache()
 
-    if not cache:
-        # Load the saved model
-        saved_model = ALSModel.load("savedALS_model")
-        cache["model"] = saved_model
-    else:
-        saved_model = cache["model"]
+        if not cache:
+            # Load the saved model
+            saved_model = ALSModel.load('D:\\savedALS_model')
+            cache["model"] = saved_model
+        else:
+            saved_model = cache["model"]
 
-    # Get the movies the user has rated
-    user_ratings = ratings.filter(col('userId') == user_id)
+        # Get the movies the user has rated
+        user_ratings = ratings.filter(col('userId') == user_id)
 
-    # Get the top 10 recommended movies for the user
-    user_recs = saved_model.recommendForAllUsers(10).filter(col('userId') == user_id)
+        # Get the top 10 recommended movies for the user
+        user_recs = saved_model.recommendForAllUsers(10).filter(col('userId') == user_id)
 
-    # Extract the recommended movie IDs and explode them into separate rows
-    user_recs = user_recs.selectExpr("explode(recommendations) as rec")
-    user_recs = user_recs.select('rec.movieId')
+        # Extract the recommended movie IDs and explode them into separate rows
+        user_recs = user_recs.selectExpr("explode(recommendations) as rec")
+        user_recs = user_recs.select('rec.movieId')
 
-    # Join with the movies DataFrame to get the movie titles
-    user_recs = user_recs.join(movies, 'movieId')
+        # Join with the movies DataFrame to get the movie titles
+        user_recs = user_recs.join(movies, 'movieId')
 
-    # Serialize the result as JSON and return it
-    result = {
-        'user_id': user_id,
-        'movies_liked': list(user_ratings.filter(col('rating') >= 4).join(movies, 'movieId').select('title').toPandas()['title']),
-        'movies_disliked': list(user_ratings.filter(col('rating') < 4).join(movies, 'movieId').select('title').toPandas()['title']),
-        'recommended_movies': list(user_recs.select('title').toPandas()['title'])
-    }
-    return JsonResponse(result)
+        # Serialize the result as JSON and return it
+        result = {
+            'user_id': user_id,
+            'movies_liked': list(user_ratings.filter(col('rating') >= 4).join(movies, 'movieId').select('title').toPandas()['title']),
+            'movies_disliked': list(user_ratings.filter(col('rating') < 4).join(movies, 'movieId').select('title').toPandas()['title']),
+            'recommended_movies': list(user_recs.select('title').toPandas()['title'])
+        }
+        return JsonResponse(result)
